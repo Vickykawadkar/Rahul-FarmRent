ed6ee7e06f88aa905c32f9939c7ce29121e70744
Vickykawadkar <vickykawadkar779@gmail.com>
2025-04-13 16:56:13 +0530

    nav links added

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -1,60 +1,92 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Search from './Search';
-import {useDispatch, useSelector} from 'react-redux';
-import {DropdownButton, Dropdown, Image} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dropdown, Image } from 'react-bootstrap';
 import { logout } from '../../actions/userActions';
 
+export default function Header() {
+  const { isAuthenticated, user } = useSelector(state => state.authState);
+  const { items: cartItems } = useSelector(state => state.cartState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default function Header () {
-    const { isAuthenticated, user } = useSelector(state => state.authState);
-    const { items:cartItems } = useSelector(state => state.cartState)
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const logoutHandler = () => {
-      dispatch(logout);
-    }
+  const logoutHandler = () => {
+    dispatch(logout);
+  };
 
+  return (
+    <nav className="navbar row align-items-center px-3">
+      {/* Left: Logo + Nav Links */}
+      <div className="col-12 col-md-5 d-flex align-items-center">
+        <div className="navbar-brand mr-4">
+          <Link to="/">
+            <img width="130px" alt="Logo" src="/images/logo2.png" />
+          </Link>
+        </div>
 
-    return (
-    <nav className="navbar row">
-        <div className="col-12 col-md-3">
-          <div className="navbar-brand">
-            <Link to="/">
-              <img width="150px" alt='JVLcart Logo' src="/images/logo2.png" />
-            </Link>
-            </div>
+        <ul className="navbar-nav d-flex flex-row ml-2">
+          <li className="nav-item mx-2">
+            <Link className="nav-link p-0" to="/">Home</Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link className="nav-link p-0" to="/products">Products</Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link className="nav-link p-0" to="/category/tractor">Tractors</Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link className="nav-link p-0" to="/contact">Contact</Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link className="nav-link p-0" to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Middle: Search */}
+      <div className="col-12 col-md-4 mt-3 mt-md-0">
+        <div style={{ maxWidth: '350px', margin: '0 auto' }}>
+          <Search />
         </div>
-  
-        <div className="col-12 col-md-6 mt-2 mt-md-0">
-           <Search/>
-        </div>
-  
-        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          { isAuthenticated ? 
-            (
-              <Dropdown className='d-inline' >
-                  <Dropdown.Toggle variant='default text-white pr-5' id='dropdown-basic'>
-                    <figure className='avatar avatar-nav'>
-                      <Image width="50px" src={user.avatar??'./images/default_avatar.png'}  />
-                    </figure>
-                    <span>{user.name}</span>
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                      { user.role === 'admin' && <Dropdown.Item onClick={() => {navigate('admin/dashboard')}} className='text-dark'>Dashboard</Dropdown.Item> }
-                      <Dropdown.Item onClick={() => {navigate('/myprofile')}} className='text-dark'>Profile</Dropdown.Item>
-                      <Dropdown.Item onClick={() => {navigate('/orders')}} className='text-dark'>Orders</Dropdown.Item>
-                      <Dropdown.Item onClick={logoutHandler} className='text-danger'>Logout</Dropdown.Item>
-                  </Dropdown.Menu>
-              </Dropdown>
-            )
-          
-          :
-            <Link to="/login"  className="btn" id="login_btn">Login</Link>
-          }
-          <Link to="/cart"><span id="cart" className="ml-3">Cart</span></Link>
-          <span className="ml-1" id="cart_count">{cartItems.length}</span>
-        </div>
+      </div>
+
+      {/* Right: Cart & Auth */}
+      <div className="col-12 col-md-3 mt-3 mt-md-0 text-center text-md-right">
+        {isAuthenticated ? (
+          <Dropdown className='d-inline'>
+            <Dropdown.Toggle variant='default text-white pr-3' id='dropdown-basic'>
+              <figure className='avatar avatar-nav'>
+                <Image width="40px" src={user.avatar ?? './images/default_avatar.png'} />
+              </figure>
+              <span>{user.name}</span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {user.role === 'admin' && (
+                <Dropdown.Item onClick={() => navigate('/admin/dashboard')} className='text-dark'>
+                  Dashboard
+                </Dropdown.Item>
+              )}
+              <Dropdown.Item onClick={() => navigate('/myprofile')} className='text-dark'>
+                Profile
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => navigate('/orders')} className='text-dark'>
+                Orders
+              </Dropdown.Item>
+              <Dropdown.Item onClick={logoutHandler} className='text-danger'>
+                Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        ) : (
+          <Link to="/login" className="btn" id="login_btn">Login</Link>
+        )}
+
+        <Link to="/cart">
+          <span id="cart" className="ml-3">Cart</span>
+        </Link>
+        <span className="ml-1" id="cart_count">{cartItems.length}</span>
+      </div>
     </nav>
-    )
+  );
 }
